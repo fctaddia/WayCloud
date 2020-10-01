22819d933a0a8f0b4ac20d9cf93ea9d6d7798d1b
Francesco Taddia <fctaddia@gmail.com>
2020-10-01 10:16:21 +0200

    Delete checker.php

--- app/handler/login/checker.php
+++
@@ -1,8 +0,0 @@
-<?php
-	if (isset($_COOKIE['email']) || !empty($_COOKIE['email'])) :
-		header('Refresh: 0; URL = myspace.php');
-		session_start();
-		$_SESSION['valid'] = true;
-		$_SESSION['timeout'] = time();
-	endif;
-?>
